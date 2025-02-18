7e1164641afb6a6315d8788ea3a9e34e25656b1d
RaildoBru <raildobruno@gmail.com>
2025-02-18 20:23:07 -0300

    ajuste linhas comentadas

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-//import sequelize from "./config/database.js";
-//import productRoutes from "./src/routes/product.routes.js";
+import sequelize from "./config/database.js";
+import productRoutes from "./src/routes/product.routes.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./src/docs/swagger.js";
 
@@ -12,14 +12,13 @@ const PORT = process.env.PORT || 3002;
 app.use(cors());
 app.use(express.json());
 
-//app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/health-check", (req, res) => {
   res.send("Health check passed!");
 });
 
-app.listen(PORT, () => console.log(`API Gateway rodando na porta ${PORT}`));
-//sequelize.authenticate().then(() => {
-  //app.listen(PORT, () => console.log(`### Servidor rodando na porta ${PORT} ###`));
-//});
+sequelize.authenticate().then(() => {
+  app.listen(PORT, () => console.log(`### Servidor rodando na porta ${PORT} ###`));
+});
